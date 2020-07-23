e2dfc4778afcce07df5ea9898129575282289b23
observant-mapping-4330 <sdonohue0918@gmail.com>
2020-07-23 00:58:58 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,8 +2,8 @@ def find_element_index(array, value_to_find)
 
   count = 0
   while count < array.length do
-    # comparison code will go here
+    puts array[count] == value_to_find
     count += 1
   end
-endolution here
+
 end
